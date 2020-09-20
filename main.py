b9a20f2daef9eff91caa521350a50c2798bb1024
NEHA2713 <68119566+NEHA2713@users.noreply.github.com>
2020-09-21 01:52:15 +0530

    Python basics

---
+++ main.py
@@ -0,0 +1,86 @@
+#print("Hello guys")
+"""a=7
+b=8
+print(a+b)"""
+
+"""name="Neha is my name"
+#print(name*3)
+print(name[1:8])"""
+
+"""number= int(input("What is your number?"))
+print("Number is: "+str(number))"""
+
+"""a="Neha"
+b=2
+print(a+str(b))"""
+
+"""a=3
+b=9
+print(b//a)"""
+
+#Data types
+#String,list,tuples,Dictionary,Set
+
+"""a="Neha"
+print(a.upper())"""
+
+#Lists- mutable,ordered
+"""
+Name=['Neha','abc','Samaksh','Rahul']
+#print(Name)
+
+Name[1]='Rhythm'
+#print(Name)
+
+print(Name[0:3])
+#add a new element list
+Name.append('Keshav')
+#print(Name)
+Name.insert(2,'Keshav')
+print(Name)
+
+Name.reverse()
+print(Name)"""
+
+"""a=3
+b=5
+print(complex(a,b))"""
+
+"""list1=['a','b','c']
+#for item in list:
+print(list(enumerate(list1)))"""
+
+"""list1=['2','b','c']
+for item in list1:
+  print(item*10)"""
+
+"""for x in range(1,11,2):
+  print(x*3)"""
+
+"""a=['Neha','Rahul','Rhythm','Samaksh']
+for x in a:
+  print(len(x))
+#print(len(a))"""
+
+#Dictionary
+"""name={'Samaksh':'Neha','Rahul':'Rhythm',1:'Python'}
+print(name['Rahul'])
+print(name.get('Rahul'))
+name['Samaksh']='Ria'
+name['SS']='Ronak'
+print(name)"""
+
+#tuple
+"""Animals=('abna','b','c','d','d','c','e')
+print(Animals)
+
+print(Animals[2])
+
+print(Animals)
+print(Animals.count('d'))
+print(len(Animals[0]))"""
+
+#set
+"""myset={10,256,890,56,89,10,10}
+print(myset)"""
+
